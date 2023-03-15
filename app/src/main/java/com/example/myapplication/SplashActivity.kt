60bdda57d6d630acb8923b205f3f2741a78375e1
mnadea001 <mathilde.nadeau@hotmail.fr>
2023-03-15 09:02:42 +0100

    splash on home

--- app/src/main/java/com/example/myapplication/SplashActivity.kt
+++ app/src/main/java/com/example/myapplication/SplashActivity.kt
@@ -1,11 +1,21 @@
 package com.example.myapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
-    }
+        Handler(Looper.getMainLooper()).postDelayed(Runnable {
+            val newIntent= Intent(application,HomeActivity::class.java)
+            startActivity(newIntent)
+        },2000)
+
+
+
+}
 }
